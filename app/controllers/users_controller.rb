120fad813a46b8c79951cc9f3fb559af1ebe648a
Mason Chang <changmason@gmail.com>
2015-01-13 19:42:47 +0800

    temp commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,17 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
+  end
+
+  def create
+
+  end
+
+  def update
+
+  end
+
+  def show
+    @user = User.find(params[:id])
   end
 end
